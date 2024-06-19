ce4efa9b7bd73714311f401997f4ace13fd457eb
vasil <whocaresm8ha@gmail.com>
2024-06-19 22:15:54 +0300

    Added repository method to fetch parents with their respective kids.

--- src/main/java/nbu/bg/logisticscompany/repository/ParentRepository.java
+++ src/main/java/nbu/bg/logisticscompany/repository/ParentRepository.java
@@ -2,6 +2,8 @@ package nbu.bg.logisticscompany.repository;
 
 import nbu.bg.logisticscompany.model.entity.Parent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +12,9 @@ import java.util.Optional;
 public interface ParentRepository extends JpaRepository<Parent, Long> {
 
     Optional<Parent> findByUsername(String username);
+
+
+    @Query("SELECT p FROM Parent p LEFT JOIN FETCH p.kids WHERE p.id = :parentId")
+    Parent findByIdWithKids(@Param("parentId") Long parentId);
 
 }
